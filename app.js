726a09671cab580935ea9c20505d97cd36619843
JoseCusimayta <josecusimaytar@gmail.com>
2021-02-16 16:26:32 -0500

    Cuarta Prueba
    revisando modulos

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require ('express');
+const morgan= require ('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
+const passport = require ('passport');
+var favicon = require('serve-favicon');
+const {options,database} = require('./keys');
+var TediousStore = require('connect-tedious')(session);
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Tercera Prueba")
+    res.send("Cuarta Prueba")
 })
 
 app.listen(process.env.PORT || 5000);
