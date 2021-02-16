f8dcc569deee9273afc7ae02026e6750804ff207
JoseCusimayta <josecusimaytar@gmail.com>
2021-02-16 16:15:50 -0500

    Probando app o index
    Revisando resultados

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', function(req,res){
-    res.send("Primera Prueba")
+    res.send("Segunda Prueba")
 })
 
 app.listen(process.env.PORT || 5000);
