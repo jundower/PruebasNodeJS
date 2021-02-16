16acd489d75853b7bca8cda26fd8872db9bfd6e2
JoseCusimayta <josecusimaytar@gmail.com>
2021-02-16 16:30:46 -0500

    quinta Prueba
    quinta prueba

--- app.js
+++ app.js
@@ -9,9 +9,29 @@ const {options,database} = require('./keys');
 var TediousStore = require('connect-tedious')(session);
 const app = express();
 
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('trust proxy', true) //Agregado para leer la ip del cliente
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs');
+
 app.get('/', function(req,res){
     res.send("Cuarta Prueba")
 })
 
-app.listen(process.env.PORT || 5000);
+// Starting
+app.listen(app.get('port'), () => {
+    console.log('Server is in port', app.get('port'));
+  });
+
+  
+// app.listen(process.env.PORT || 5000);
 module.exports.app;
